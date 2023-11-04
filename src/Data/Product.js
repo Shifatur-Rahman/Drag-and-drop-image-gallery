5454638a3d3bbb0529ac3a478d519c97a1433b85
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-05 00:38:48 +0600

    product add

---
+++ src/Data/Product.js
@@ -0,0 +1,60 @@
+import image1 from "../assets/images/image-1.webp"
+import image2 from "../assets/images/image-2.webp"
+import image3 from "../assets/images/image-3.webp"
+import image4 from "../assets/images/image-4.webp"
+import image5 from "../assets/images/image-5.webp"
+import image6 from "../assets/images/image-6.webp"
+import image7 from "../assets/images/image-7.webp"
+import image8 from "../assets/images/image-8.webp"
+import image9 from "../assets/images/image-9.webp"
+import image10 from "../assets/images/image-10.jpeg"
+import image11 from "../assets/images/image-11.jpeg"
+
+const Products=[
+    {
+        id: "one",
+        thumb: image1
+    },
+    {
+        id: "two",
+        thumb: image2
+    },
+    {
+        id: "three",
+        thumb: image3
+    },
+    {
+        id: "four",
+        thumb: image4
+    },
+    {
+        id: "five",
+        thumb: image5
+    },
+    {
+        id: "six",
+        thumb: image6
+    },
+    {
+        id: "seven",
+        thumb: image7
+    },
+    {
+        id: "eight",
+        thumb: image8
+    },
+    {
+        id: "nine",
+        thumb: image9
+    },
+    {
+        id: "ten",
+        thumb: image10
+    },
+    {
+        id: "elevene",
+        thumb: image11
+    }
+]
+
+export default Products;
---
+++ src/components/DisplayProduct/DisplayProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayProduct = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default DisplayProduct
