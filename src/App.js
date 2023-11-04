c470af2e0dffa7d97af989d159e0082560a8d224
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-05 00:50:28 +0600

    dependencies add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DisplayProduct from './components/DisplayProduct/DisplayProduct';
 
 function App() {
   return (
     <div className="App">
-      
+      <DisplayProduct />
     </div>
   );
 }
