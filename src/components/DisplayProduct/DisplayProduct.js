e36053ed6c1c1c5f09be96ba13fe129503d053ac
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-05 22:57:11 +0600

    hover effect add

--- src/components/DisplayProduct/DisplayProduct.js
+++ src/components/DisplayProduct/DisplayProduct.js
@@ -12,9 +12,18 @@ const DisplayProduct = () => {
     const onSortEnd = (oldIndex, newIndex) => {
         setItems((array) => arrayMove(array, oldIndex, newIndex));
     };
-    
+
+    // product cont
+  let handleSelect = (id) => {
+    console.log(id);
+  }
+  
+
+
   return (
     <>
+    <p className='title'>Gallery</p>
+    <hr />
     <SortableList
       onSortEnd={onSortEnd}
       className={classes.root}
@@ -23,18 +32,17 @@ const DisplayProduct = () => {
       {items.map(({ id, thumb },index) => (
         <SortableItem key={id}>
           <div className={`${classes.item} ${index === 0 ? classes.firstItem : ''}`}
-          // className={classes.item}
+          onClick={handleSelect(id)}
           >
             <img
-              // className={classes.image}
-              className={`${classes.image} ${index === 0 ? classes.firstImage : ''}`}
+              className={classes.image}
               alt={id}
               src={thumb}
-              imgProps={{ draggable: false }}
             />
             </div>
         </SortableItem>
       ))}
+            
     </SortableList>
 
     </>
@@ -44,10 +52,6 @@ const DisplayProduct = () => {
 export default DisplayProduct
 
 const gridStyle = makeStyles({
-  // firstItem: {
-  //   gridTemplateColumns: "2fr",
-  //   gridTemplateRows: "2fr"
-  // },
   root: {
     margin: "10px",
     display: "grid",
@@ -56,19 +60,35 @@ const gridStyle = makeStyles({
   },
   item: {
     border: "1px solid #bdc3c7",
-    borderRadius: "10px"
+    borderRadius: "10px",
   },
   image: {
     width: "100%",
     height: "100%",
-    borderRadius: "10px"
+    borderRadius: "10px",
+    "&:hover": {
+      opacity: 0.2,
+      backgroundColor: "rgba(0,0,0,0.3)"
+    }
   },
   firstItem: {
     gridRowStart: 1,
     gridRowEnd: 3,
     gridColumnStart: 1,
     gridColumnEnd: 3 
-      /* grid-row-start: 1;
-            grid-row-end: 3; */
   },
+  imageCheckbox: {
+    display: "none",
+    position: "absolute",
+    top: "5px",
+    left: "5px"
+  },
+  imageContainer:{
+  "&:hover":{
+    display: "block"
+
+  }
+  } 
+  
 });
+
